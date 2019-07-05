66f33576c26499d71f883d7387aaa9cbf19fc213
Oliver Krammer <o.k@i-kiu.at>
2019-07-05 11:15:22 +0200

    [TASK] Clean Up and Copy Pageid to clipboard

--- popup.js
+++ popup.js
@@ -7,8 +7,16 @@ let uid=0;
 
 let pageid = document.getElementById('pageid');
 
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  var currTab = tabs[0];
+  console.log(tabs);
+  console.log(currTab);
+  if (currTab) { // Sanity check
+    /* do stuff */
+  }
+});
 chrome.storage.local.get('pageid', function(data) {
-  console.log(data.pageid);
+  //console.log(data.pageid);
   pageid.innerHTML=data.pageid;
 });
 
@@ -17,4 +25,32 @@ chrome.extension.onMessage.addListener(function(message, sender) {
   console.log('onMessage Popup');
   uid=message;
   pageid.innerHTML=message;
+});
+
+
+$('#pageid').on('click', function() {
+  console.log(this);
+  var range = document.createRange();
+  console.log(range);
+  range.selectNode(this);
+  console.log(range);
+  window.getSelection().addRange(range);
+
+  var successful = false;
+  try {
+    // Now that we've selected the anchor text, execute the copy command
+    successful = document.execCommand('copy');
+    console.log(successful);
+  } catch(err) {
+    successful = false;
+    // whatever
+  }
+
+  if (!successful) {
+    prompt("Your browser does not support insta-copy. Sorry.", $(this).text());
+  }
+
+  // Remove the selections - NOTE: Should use
+  // removeRange(range) when it is supported
+  window.getSelection().removeAllRanges();
 });
